ab176f72abed602148c8ab4986457fbaa1824cc3
Wei-Cheng Yang <rainyday309@gmail.com>
2015-09-07 15:09:40 +0800

    add and remove small block by click

--- calendarScript.js
+++ calendarScript.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
 
 	// event: click to make box change//
 	$('.box').click(function(){
-		// click to make color blue //
-		$(this).toggleClass("marked");
+		if ($('.mark',this).length) {
+			$('.mark',this).remove();
+		}
+		else
+		{
+			$(this).append("<div class='mark'></div>");
+		}
+
 	});
 });	
